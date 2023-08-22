5f9fb51f510dccf22376f3ef80bcc9746c6f3f5d
cairon-henrique-60 <caironhenrique60@gmail.com>
2023-08-22 07:17:12 -0300

    Changing bar chart

--- data/__init__.py
+++ data/__init__.py
@@ -7,6 +7,7 @@ from azure.models.workItem import WorkItem
 
 
 class Data:
+    """
     def __init__(self, payload: list[WorkItem]):
         self.__init__ = self
         self.data: DataFrame = self.__convertToList(payload)
@@ -22,9 +23,43 @@ class Data:
         values = self.data.query(f"workItemType == 'Bug'", inplace=True)
         print(types, values)
 
-        # graph = sns.barplot(x="workItemType", y="quantity", color="blue")
-        # graph.set_title("Work item x Tipo")
-        # plt.show(graph)
+        graph = sns.barplot(x=types, y=values, color="blue")
+        graph.set_title("Work item x Tipo")
+        return plt.show(graph)
+
+    def perClient(self, client: str):
+        data = self.data
+        print(data)
+"""
+    def __init__(self, payload: list[WorkItem]):
+        self.data = self.__convertToList(payload)
+
+    def __convertToList(self, objs: list[WorkItem]) -> pd.DataFrame:
+        dicts = []
+        for item in objs:
+            dicts.append({**item.__dict__, "quantity": 1})
+        return pd.DataFrame(dicts)
+
+    def dashboardTotalItems(self):
+        # Agrupar por tipo de trabalho e contar a quantidade de ocorrências
+        type_counts = self.data["workItemType"].value_counts()
+
+        # Criar o gráfico de barras
+        sns.set_style("whitegrid")
+        plt.figure(figsize=(10, 6))
+        graph = sns.barplot(x=type_counts.index, y=type_counts.values, color="blue")
+        graph.set_title("Work item x Tipo")
+        graph.set_xlabel("Types of Work")
+        graph.set_ylabel("Frequency")
+        # Ajustar a margem inferior para dar espaço ao título
+        plt.subplots_adjust(bottom=0.2)
+        # Alinhar os rótulos no eixo x
+        plt.xticks(rotation=45, ha="right")
+        for i, v in enumerate(type_counts.values):
+            # Adicionar os valores no topo das barras // MEU LINDÃO
+            graph.text(i, v + 1, str(v), color='black', ha='center')
+        return plt.show()
+
 
     def perClient(self, client: str):
         data = self.data
